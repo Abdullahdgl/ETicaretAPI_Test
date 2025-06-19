5f366bf44b4a9fc2701ea66e23756de3adb008ae
Abdullah Dagli <abdullah.dagli@outlook.com>
2025-06-19 21:39:15 +0300

    Tracking 1 commit

--- Infrastructure/ETicaretAPI.Persistance/Repositories/ReadRepository.cs
+++ Infrastructure/ETicaretAPI.Persistance/Repositories/ReadRepository.cs
@@ -22,18 +22,30 @@ namespace ETicaretAPI.Persistance.Repositories
 
 		public DbSet<T> Table => _context.Set<T>();
 
-		public IQueryable<T> GetAll()
-			=> Table;
+		public IQueryable<T> GetAll(bool tracking = true)
+		{
+			var query = Table.AsQueryable();
+			if (!tracking)
+				query =	query.AsNoTracking();
+			return query;
+
+						
+		}
 
 
-		public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
-			=> Table.Where(method);
+		public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
+		{
+			var query = Table.Where(method);
+			if(!tracking)
+				query = query.AsNoTracking();
+			return query;
+		}
 
-		public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+		public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
 			=> await Table.FirstOrDefaultAsync(method);
 
 		//find metodu ile 
-		public async Task<T> GetByIdAsync(string id)
+		public async Task<T> GetByIdAsync(string id, bool tracking = true)
 			=> await Table.FindAsync(Guid.Parse(id));
 
 
